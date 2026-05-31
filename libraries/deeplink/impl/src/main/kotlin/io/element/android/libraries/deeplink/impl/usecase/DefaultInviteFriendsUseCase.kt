d8b4004d65f8b8a2232d906ae6b6a3a290e5be84
Davy <hdavy2002@gmail.com>
2026-05-31 12:50:18 +0530

    AvaTok: avatok.ai App Link invites (open app + start chat, download fallback)

--- libraries/deeplink/impl/src/main/kotlin/io/element/android/libraries/deeplink/impl/usecase/DefaultInviteFriendsUseCase.kt
+++ libraries/deeplink/impl/src/main/kotlin/io/element/android/libraries/deeplink/impl/usecase/DefaultInviteFriendsUseCase.kt
@@ -15,10 +15,8 @@ import io.element.android.libraries.core.meta.BuildMeta
 import io.element.android.libraries.deeplink.api.usecase.InviteFriendsUseCase
 import io.element.android.libraries.di.SessionScope
 import io.element.android.libraries.matrix.api.MatrixClient
-import io.element.android.libraries.matrix.api.permalink.PermalinkBuilder
 import io.element.android.libraries.ui.strings.CommonStrings
 import io.element.android.services.toolbox.api.strings.StringProvider
-import timber.log.Timber
 import io.element.android.libraries.androidutils.R as AndroidUtilsR
 
 @ContributesBinding(SessionScope::class)
@@ -26,24 +24,20 @@ class DefaultInviteFriendsUseCase(
     private val stringProvider: StringProvider,
     private val matrixClient: MatrixClient,
     private val buildMeta: BuildMeta,
-    private val permalinkBuilder: PermalinkBuilder,
 ) : InviteFriendsUseCase {
     override fun execute(activity: Activity) {
-        val permalinkResult = permalinkBuilder.permalinkForUser(matrixClient.sessionId)
-        permalinkResult.fold(
-            onSuccess = { permalink ->
-                val appName = buildMeta.applicationName
-                activity.startSharePlainTextIntent(
-                    activityResultLauncher = null,
-                    chooserTitle = stringProvider.getString(CommonStrings.action_invite_friends),
-                    text = stringProvider.getString(CommonStrings.invite_friends_text, appName, permalink),
-                    extraTitle = stringProvider.getString(CommonStrings.invite_friends_rich_title, appName),
-                    noActivityFoundMessage = stringProvider.getString(AndroidUtilsR.string.error_no_compatible_app_found)
-                )
-            },
-            onFailure = {
-                Timber.e(it)
-            }
+        // AvaTok: share a clean, branded avatok.ai invite link (an Android App Link that
+        // opens AvaTok directly, or a download page if the app isn't installed) instead of
+        // a matrix.to permalink that lands on a confusing Matrix web picker.
+        val localpart = matrixClient.sessionId.value.removePrefix("@").substringBefore(":")
+        val permalink = "https://avatok.ai/i/$localpart"
+        val appName = buildMeta.applicationName
+        activity.startSharePlainTextIntent(
+            activityResultLauncher = null,
+            chooserTitle = stringProvider.getString(CommonStrings.action_invite_friends),
+            text = stringProvider.getString(CommonStrings.invite_friends_text, appName, permalink),
+            extraTitle = stringProvider.getString(CommonStrings.invite_friends_rich_title, appName),
+            noActivityFoundMessage = stringProvider.getString(AndroidUtilsR.string.error_no_compatible_app_found)
         )
     }
 }
